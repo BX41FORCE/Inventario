e971441f0d1d89d13194fc98ce479bbbfc85a121
JosseAguirre <jac.aguirre@yavirac.edu.ec>
2018-09-18 18:03:45 -0500

    Web Service

---
+++ ModuloInventario/ModuloInventario/DBContext/ASIGNACIONCOMPUTADORES.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ModuloInventario.DBContext
+{
+    public class ASIGNACIONCOMPUTADORES
+    {
+        public int  SECUENCIAL { get; set; }
+        public string RESPONSABLE { get; set; }
+        public int SECUANCIALPERSONA { get; set; }
+        public int SECUANCIALINGRESOCOMPUTADORES { get; set; }
+        public int TIEMPOINICIO { get; set; }
+        public int TIEMPOFIN { get; set; }
+    }
+}
