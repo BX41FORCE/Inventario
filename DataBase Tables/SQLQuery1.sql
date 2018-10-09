75a50f92c82a7aeccc24eac314bc2679e34aeb57
JosseAguirre <jac.aguirre@yavirac.edu.ec>
2018-10-09 14:16:13 -0500

    Comete este v2.0

---
+++ DataBase Tables/SQLQuery1.sql
@@ -0,0 +1,16 @@
+INSERT INTO INVENTARIO.asignacionComputadores VALUES
+('HOLA','UIO1','09-10-2018','10-10-2018'),
+('HOLA','UIO2','09-10-2018','10-10-2018'),
+('HOLA','UIO3','09-10-2018','10-10-2018'),
+('HOLA','UIO4','09-10-2018','10-10-2018');
+
+INSERT INTO INVENTARIO.ingresoComputadores VALUES
+('UIO3','Quito','122345',12,'hola',23,1,0,'gato','123','perro','ninguna',1,1223,12233,'09-10-2018','foco'),
+('UIO4','Quito','122345',12,'hola',23,1,0,'gato','123','perro','ninguna',1,1223,12233,'09-10-2018','foco'),
+('UIO5','Quito','122345',12,'hola',23,1,0,'gato','123','perro','ninguna',1,1223,12233,'09-10-2018','foco'),
+('UIO6','Quito','122345',12,'hola',23,1,0,'gato','123','perro','ninguna',1,1223,12233,'09-10-2018','foco'),
+('UIO7','Quito','122345',12,'hola',23,1,0,'gato','123','perro','ninguna',1,1223,12233,'09-10-2018','foco'),
+('UIO8','Quito','122345',12,'hola',23,1,0,'gato','123','perro','ninguna',1,1223,12233,'09-10-2018','foco'),
+('UIO9','Quito','122345',12,'hola',23,1,0,'gato','123','perro','ninguna',1,1223,12233,'09-10-2018','foco'),
+('UIO10','Quito','122345',12,'hola',23,1,0,'gato','123','perro','ninguna',1,1223,12233,'09-10-2018','foco'),
+('UIO11','Quito','122345',12,'hola',23,1,0,'gato','123','perro','ninguna',1,1223,12233,'09-10-2018','foco');
