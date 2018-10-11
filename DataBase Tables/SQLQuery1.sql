6f99f2468839062aa8601b0c010f168957bd2bc1
JosseAguirre <jac.aguirre@yavirac.edu.ec>
2018-10-11 11:50:42 -0500

    Cambios pequeños v1.1

--- DataBase Tables/SQLQuery1.sql
+++ DataBase Tables/SQLQuery1.sql
@@ -1,16 +1,46 @@
 INSERT INTO INVENTARIO.asignacionComputadores VALUES
-('HOLA','UIO1','09-10-2018','10-10-2018'),
-('HOLA','UIO2','09-10-2018','10-10-2018'),
-('HOLA','UIO3','09-10-2018','10-10-2018'),
-('HOLA','UIO4','09-10-2018','10-10-2018');
+('Luis','UIO5','09-10-2018','10-10-2018'),
+('Juan','UIO6','09-10-2018','10-10-2018'),
+('Pepe','UIO7','09-10-2018','10-10-2018'),
+('Felipe','UIO8','09-10-2018','10-10-2018');
 
 INSERT INTO INVENTARIO.ingresoComputadores VALUES
-('UIO3','Quito','122345',12,'hola',23,1,0,'gato','123','perro','ninguna',1,1223,12233,'09-10-2018','foco'),
-('UIO4','Quito','122345',12,'hola',23,1,0,'gato','123','perro','ninguna',1,1223,12233,'09-10-2018','foco'),
-('UIO5','Quito','122345',12,'hola',23,1,0,'gato','123','perro','ninguna',1,1223,12233,'09-10-2018','foco'),
-('UIO6','Quito','122345',12,'hola',23,1,0,'gato','123','perro','ninguna',1,1223,12233,'09-10-2018','foco'),
-('UIO7','Quito','122345',12,'hola',23,1,0,'gato','123','perro','ninguna',1,1223,12233,'09-10-2018','foco'),
-('UIO8','Quito','122345',12,'hola',23,1,0,'gato','123','perro','ninguna',1,1223,12233,'09-10-2018','foco'),
-('UIO9','Quito','122345',12,'hola',23,1,0,'gato','123','perro','ninguna',1,1223,12233,'09-10-2018','foco'),
 ('UIO10','Quito','122345',12,'hola',23,1,0,'gato','123','perro','ninguna',1,1223,12233,'09-10-2018','foco'),
-('UIO11','Quito','122345',12,'hola',23,1,0,'gato','123','perro','ninguna',1,1223,12233,'09-10-2018','foco');
+('UIO11','Quito','122345',12,'hola',23,1,0,'gato','123','perro','ninguna',1,1223,12233,'09-10-2018','foco'),
+('UIO12','Quito','122345',12,'hola',23,1,0,'gato','123','perro','ninguna',1,1223,12233,'09-10-2018','foco'),
+('UIO13','Quito','122345',12,'hola',23,1,0,'gato','123','perro','ninguna',1,1223,12233,'09-10-2018','foco'),
+('UIO14','Quito','122345',12,'hola',23,1,0,'gato','123','perro','ninguna',1,1223,12233,'09-10-2018','foco'),
+('UIO15','Quito','122345',12,'hola',23,1,0,'gato','123','perro','ninguna',1,1223,12233,'09-10-2018','foco'),
+('UIO16','Quito','122345',12,'hola',23,1,0,'gato','123','perro','ninguna',1,1223,12233,'09-10-2018','foco'),
+('UIO17','Quito','122345',12,'hola',23,1,0,'gato','123','perro','ninguna',1,1223,12233,'09-10-2018','foco'),
+('UIO18','Quito','122345',12,'hola',23,1,0,'gato','123','perro','ninguna',1,1223,12233,'09-10-2018','foco');
+
+INSERT INTO INVENTARIO.item VALUES
+('UIO-EC-','Laptop','Equipo de Computo'),
+('UIO-EC-','CPU','Equipo de Computo'),
+('UIO-EC-','Disco Duro Externo','Equipo de Computo'),
+('UIO-EC-','Pantalla','Equipo de Computo'),
+('UIO-EC-','Mouse','Equipo de Computo'),
+('UIO-EC-','Impresora','Equipo de Computo'),
+('UIO-EC-','Teclado','Equipo de Computo'),
+('UIO-EC-','Web Cam','Equipo de Computo'),
+('UIO-EC-','Router','Equipo de Computo'),
+('UIO-EC-','Flash Memory','Equipo de Computo'),
+('UIO-EC-','Regulador','Equipo de Computo');
+
+INSERT INTO INVENTARIO.ingresoVarios VALUES
+('UIO-OF-','Quito','Prueba','hola','gato','123','prueba ',1,12312,2312,'11/10/2018','dsdasdas'),
+('UIO-OF-','Quito','Prueba','hola','gato','123','prueba ',1,12312,2312,'11/10/2018','dsdasdas'),
+('UIO-OF-','Quito','Prueba','hola','gato','123','prueba ',1,12312,2312,'11/10/2018','dsdasdas'),
+('UIO-OF-','Quito','Prueba','hola','gato','123','prueba ',1,12312,2312,'11/10/2018','dsdasdas'),
+('UIO-OF-','Quito','Prueba','hola','gato','123','prueba ',1,12312,2312,'11/10/2018','dsdasdas'),
+('UIO-OF-','Quito','Prueba','hola','gato','123','prueba ',1,12312,2312,'11/10/2018','dsdasdas'),
+('UIO-OF-','Quito','Prueba','hola','gato','123','prueba ',1,12312,2312,'11/10/2018','dsdasdas'),
+('UIO-OF-','Quito','Prueba','hola','gato','123','prueba ',1,12312,2312,'11/10/2018','dsdasdas'),
+('UIO-OF-','Quito','Prueba','hola','gato','123','prueba ',1,12312,2312,'11/10/2018','dsdasdas'),
+('UIO-OF-','Quito','Prueba','hola','gato','123','prueba ',1,12312,2312,'11/10/2018','dsdasdas'),
+('UIO-OF-','Quito','Prueba','hola','gato','123','prueba ',1,12312,2312,'11/10/2018','dsdasdas'),
+('UIO-OF-','Quito','Prueba','hola','gato','123','prueba ',1,12312,2312,'11/10/2018','dsdasdas'),
+('UIO-OF-','Quito','Prueba','hola','gato','123','prueba ',1,12312,2312,'11/10/2018','dsdasdas'),
+('UIO-OF-','Quito','Prueba','hola','gato','123','prueba ',1,12312,2312,'11/10/2018','dsdasdas');
+
